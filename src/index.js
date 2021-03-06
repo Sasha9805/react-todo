42c722be2b51e12db813813aa67dac155e72150c
Sasha9805 <pimenovs33@gmail.com>
2021-03-06 16:49:56 +0200

    Добавлен React-элемент на страницу

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const el = React.createElement(
+//   'h1',
+//   null,
+//   'Hello World'
+// );
+
+// const el = <h1>Hello World</h1>;
+// Маленький (мало свойств) легковесный объект
+// Исп. в VirtualDOM. Поэтому React работает так быстро
+// console.log(el);
+
+const el = (
+  <div>
+    <h1>
+      My Todo List
+    </h1>
+    <input type="text" placeholder="search"/>
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(
+  el,
+  document.getElementById('root')
+);
