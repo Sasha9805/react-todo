8e24797667657ebcebe7b8b432806508bb87b3d8
Sasha9805 <pimenovs33@gmail.com>
2021-03-10 15:09:07 +0200

    Компоненты-фи фильтр и задача преобразованы в компоненты-классы

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
+export default class ItemStatusFilter extends Component {
 
-  return (
-    <div className="btn-group">
-      <button className="btn btn-info">All</button>
-      <button className="btn btn-outline-secondary">Active</button>
-      <button className="btn btn-outline-secondary">Done</button>
-    </div>
-  );
+  render() {
 
-};
+    return (
+      <div className="btn-group">
+        <button className="btn btn-info">All</button>
+        <button className="btn btn-outline-secondary">Active</button>
+        <button className="btn btn-outline-secondary">Done</button>
+      </div>
+    );
 
-export default ItemStatusFilter;
+  }
+
+}
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends Component {
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+  render() {
 
-  return (
-    <span className="todo-list-item d-flex justify-content-between">
-      <span className="todo-list-item-label"
-            style={ style }>
-        { label }
+    const { label, important = false } = this.props;
+
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+
+    return (
+      <span className="todo-list-item d-flex justify-content-between">
+
+        <span className="todo-list-item-label"
+              style={ style }>
+          { label }
+        </span>
+
+        <span className="todo-list-item-buttons">
+          <button className="btn btn-sm btn-outline-success">
+            <i className="fa fa-exclamation"></i>
+          </button>
+
+          <button className="btn btn-sm btn-outline-danger">
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </span>
+
       </span>
+    );
 
-      <span className="todo-list-item-buttons">
-        <button className="btn btn-sm btn-outline-success">
-          <i className="fa fa-exclamation"></i>
-        </button>
+  }
 
-        <button className="btn btn-sm btn-outline-danger">
-          <i className="fa fa-trash-o"></i>
-        </button>
-      </span>
-
-    </span>
-
-  );
-};
-
-export default TodoListItem;
+}
