fe91d1b2765210ab05558721bf706963cc1d024d
Sasha9805 <pimenovs33@gmail.com>
2021-03-10 21:15:47 +0200

    Добавлена функциональность для отмечания пункта как важный

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,43 +4,53 @@ import './todo-list-item.css';
 export default class TodoListItem extends Component {
 
   state = {
-    done: false
+    done: false,
+    important: false
   };
 
   onLabelClick = () => {
-    // console.log(`done: ${this.props.label}`);
     this.setState({
-      done: true
+      done: true,
+    });
+  };
+
+  onMarkImportant = () => {
+    this.setState({
+      important: true
     });
   };
 
   render() {
 
-    const { label, important = false } = this.props;
+    const { label } = this.props;
 
-    const { done } = this.state;
+    const { done, important } = this.state;
 
     let classNames = 'todo-list-item d-flex justify-content-between';
     if (done) {
       classNames += ' done';
     }
+    if (important) {
+      classNames += ' important';
+    }
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    // const style = {
+    //   color: important ? 'steelblue' : 'black',
+    //   fontWeight: important ? 'bold' : 'normal'
+    // };
 
     return (
       <span className={ classNames }>
 
         <span className="todo-list-item-label"
-              style={ style }
               onClick={ this.onLabelClick }>
           { label }
         </span>
 
         <span className="todo-list-item-buttons">
-          <button className="btn btn-sm btn-outline-success">
+          <button
+              className="btn btn-sm btn-outline-success"
+              onClick={ this.onMarkImportant }>
             <i className="fa fa-exclamation"></i>
           </button>
 
