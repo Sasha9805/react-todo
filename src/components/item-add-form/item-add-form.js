239115d522b59b1fe48396731296d26bf806dc67
Sasha9805 <pimenovs33@gmail.com>
2021-03-13 12:14:08 +0200

    Эл-т input сделали контролируемым из компонента ItemAddForm

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -21,6 +21,9 @@ export default class ItemAddForm extends Component {
       return;
     }
     this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ''
+    });
   };
 
   render() {
@@ -34,6 +37,7 @@ export default class ItemAddForm extends Component {
           className="form-control"
           onChange={this.onLabelChange}
           placeholder="What needs to be done"
+          value={this.state.label}
         />
         <button
           className="btn btn-outline-secondary">
